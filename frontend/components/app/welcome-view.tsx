7106e0631639fd4bea98318b5051f13bd9f26863
Takshak Rajawat <155869988+icvbt@users.noreply.github.com>
2025-11-30 11:18:35 +0530

    Update welcome-view.tsx

--- frontend/components/app/welcome-view.tsx
+++ frontend/components/app/welcome-view.tsx
@@ -34,7 +34,7 @@ export const WelcomeView = ({
         <WelcomeImage />
 
         <p className="text-foreground max-w-prose pt-1 leading-6 font-medium">
-          Chat live with Guts the Lighthouse Dungeon Game Master
+          Welcome to Mad Max Store, check out our new winter collection!
         </p>
 
         <Button variant="primary" size="lg" onClick={onStartCall} className="mt-6 w-64 font-mono">
